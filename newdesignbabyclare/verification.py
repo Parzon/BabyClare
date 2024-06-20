bc94e16860ac7ed30c09355654c7c283fcbc8c3a
Parzon <124113141+Parzon@users.noreply.github.com>
2024-06-20 11:40:45 +0530

    updated reqs

---
+++ newdesignbabyclare/verification.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import sounddevice as sd
+import wavio
+import torch
+import torchaudio
+import scipy
+import requests
+import hume
+import keras
+import keras_vggface
+from pydub import AudioSegment
+from dotenv import load_dotenv
+import pytest
+
+print("All modules imported successfully.")
